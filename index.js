2ecc946b22c74ba9d99bb0a40b210120607dee58
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2023-07-03 17:58:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'host1',
+  password: 'host1',
+  database: 'grocery_store',
+  port: 1433
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+
+app.get('/items', (req, res) => {
+  connection.query('SELECT * FROM items', (err, results) => {
+    if (err) {
+      console.error('Error querying the database: ', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.json(results);
+    console.log("data fetched");
+  });
+});
+
+app.post('/add-item', (req, res) => {
+    const { itemName, itemPrice, itemImage } = req.body;
+  
+    connection.query('INSERT INTO items (itemName, itemPrice, itemImage) VALUES (?, ?, ?)', [itemName, itemPrice, itemImage], (err) => {
+      if (err) {
+        console.error('Error inserting new item: ', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+  
+      res.sendStatus(200);
+    });
+  });
+
+app.post('/add-to-cart', (req, res) => {
+  const { itemName, itemPrice } = req.body;
+  connection.query('SELECT * FROM cart_items WHERE itemName = ?', [itemName], (err, results) => {
+    if (err) {
+      console.error('Error querying the database: ', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    if (results.length > 0) {
+      const quantity = results[0].quantity + 1;
+      connection.query('UPDATE cart_items SET quantity = ? WHERE itemName = ?', [quantity, itemName], (err) => {
+        if (err) {
+          console.error('Error updating the quantity: ', err);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.sendStatus(200);
+        console.log("Updated qty");
+      });
+    } else {
+      connection.query('INSERT INTO cart_items (itemName, itemPrice, quantity) VALUES (?, ?, 1)', [itemName, itemPrice], (err) => {
+        if (err) {
+          console.error('Error inserting new item: ', err);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.sendStatus(200);
+        console.log("Inserted new item");
+      });
+    }
+  });
+});
+
+app.post('/checkout', (req, res) => {
+    const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const items = req.body.items;
+  
+    // Generate bill logic here
+    let total = 0;
+    let billItems = '';
+  
+    items.forEach(item => {
+      const itemTotal = item.itemPrice * item.quantity;
+      total += itemTotal;
+      billItems += `${item.itemName} - $${item.itemPrice} x ${item.quantity} = $${itemTotal}\n`;
+    });
+  
+    // Add the bill record to the orders table
+    connection.query('INSERT INTO orders (billItems, total, date) VALUES (?, ?, ?)', [billItems, total, date], (err) => {
+      if (err) {
+        console.error('Error inserting new order: ', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      
+      // Clear the cart_items table
+      connection.query('DELETE FROM cart_items', (err) => {
+        if (err) {
+          console.error('Error clearing cart items: ', err);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+  
+        res.sendStatus(200);
+        console.log("cleared and created order");
+      });
+    });
+  });
+
+  app.get('/cart-items', (req, res) => {
+    connection.query('SELECT * FROM cart_items', (err, results) => {
+      if (err) {
+        console.error('Error querying the database: ', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      res.json(results);
+    });
+  });
+
+app.get('/orders', (req, res) => {
+    connection.query('SELECT * FROM orders', (err, results) => {
+      if (err) {
+        console.error('Error querying the database: ', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      res.json(results);
+    });
+  });
+    
+
+app.listen(8000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ public/script.js
@@ -0,0 +1,163 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let addToCartButtons = document.querySelectorAll(".add-to-cart");
+    const cartItems = document.getElementById("cart-items");
+    const checkoutBtn = document.getElementById("checkout-btn");
+    const addItemForm = document.getElementById("add-item-form");
+  
+    const API_URL = "";
+  
+    function fetchItems() {
+      fetch(`${API_URL}/items`)
+        .then(response => response.json())
+        .then(data => {
+          data.forEach(item => {
+            const newItem = document.createElement("div");
+            newItem.className = "item";
+            newItem.innerHTML = `
+              <img src="${item.itemImage}" alt="${item.itemName}">
+              <h3>${item.itemName}</h3>
+              <p>$${item.itemPrice}</p>
+              <button class="add-to-cart">Add to Cart</button>
+            `;
+            document.querySelector(".grid").appendChild(newItem);
+            addToCartButtons = document.querySelectorAll(".add-to-cart");
+            addToCartButtons.forEach(function(button) {
+                button.addEventListener("click", function() {
+                  const item = button.parentNode;
+                  const itemName = item.querySelector("h3").innerText;
+                  const itemPrice = parseFloat(item.querySelector("p").innerText.replace("$", ""));
+                  addToCart(itemName, itemPrice);
+                });
+              });
+          });
+        })
+        .catch(error => {
+          console.error("Error fetching items: ", error);
+        });
+        addToCartButtons = document.querySelector(".add-to-cart");
+    }
+  
+    function fetchCartItems() {
+      fetch(`${API_URL}/cart-items`)
+        .then(response => response.json())
+        .then(data => {
+          cartItems.innerHTML = "";
+          data.forEach(item => {
+            const li = document.createElement("li");
+            li.innerText = `${item.itemName} - $${item.itemPrice} x ${item.quantity}`;
+            cartItems.appendChild(li);
+          });
+        })
+        .catch(error => {
+          console.error("Error fetching cart items: ", error);
+        });
+    }
+  
+    function addToCart(itemName, itemPrice) {
+      fetch(`${API_URL}/add-to-cart`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          itemName: itemName,
+          itemPrice: itemPrice
+        })
+      })
+        .then(response => {
+          if (response.ok) {
+            fetchCartItems();
+          } else {
+            console.error("Error adding item to cart.");
+          }
+        })
+        .catch(error => {
+          console.error("Error adding item to cart: ", error);
+        });
+    }
+  
+
+  
+    checkoutBtn.addEventListener("click", function() {
+      generateBill();
+      fetchOrders();
+    });
+  
+    addItemForm.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const itemNameInput = document.getElementById("item-name");
+      const itemPriceInput = document.getElementById("item-price");
+  
+      const itemName = itemNameInput.value;
+      const itemPrice = parseFloat(itemPriceInput.value);
+  
+      addToCart(itemName, itemPrice);
+  
+      itemNameInput.value = "";
+      itemPriceInput.value = "";
+    });
+      // ...
+      
+      function generateBill() {
+        fetch(`${API_URL}/cart-items`)
+          .then(response => response.json())
+          .then(data => {
+            if (data.length === 0) {
+              alert("Your cart is empty.");
+              return;
+            }
+      
+            fetch(`${API_URL}/checkout`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({
+                items: data
+              })
+            })
+              .then(response => {
+                if (response.ok) {
+                  alert("Bill generated and cart cleared.");
+                  cartItems.innerHTML = "";
+                } else {
+                  console.error("Error generating bill.");
+                }
+              })
+              .catch(error => {
+                console.error("Error generating bill: ", error);
+              });
+          })
+          .catch(error => {
+            console.error("Error fetching cart items: ", error);
+          });
+      }
+    fetchItems();
+    fetchCartItems();
+
+
+    const orderList = document.getElementById("orderList");
+
+    function fetchOrders() {
+      fetch(`${API_URL}/orders`)
+        .then(response => response.json())
+        .then(data => {
+          orderList.innerHTML = "";
+          data.forEach(order => {
+            const li = document.createElement("li");
+            li.innerText = `Order ID: ${order.id}, Total: $${order.total}, Date: ${order.date}`;
+            orderList.appendChild(li);
+          });
+        })
+        .catch(error => {
+          console.error("Error fetching orders: ", error);
+        });
+    }
+    
+    fetchOrders();
+    
+
+  });
+  
+ 
+  
